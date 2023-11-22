6194c966d97851b053c92a3886112796c2e82dd1
Pedro Luiz Vidal Athayde <130158934+PedroLVA@users.noreply.github.com>
2023-11-22 15:15:54 -0300

    Added new code

---
+++ conexao.php
@@ -0,0 +1,20 @@
+<?php
+
+ $servidor = 'fdb1034.awardspace.net';
+ $db_nome = '4045440_oleodobem';
+ $db_senha = 'FutureMinds2023??';
+ $db_usuario = '4045440_oleodobem';
+ 
+ $conexao = new mysqli($servidor,$db_nome,$db_senha,$db_usuario);
+
+/*
+if($conexao->connect_errno)
+ {
+ echo "erro";
+ }
+ else
+ {
+ echo "conexão efetuada com sucesso";
+ }
+ */
+?>
